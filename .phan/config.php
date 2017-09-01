4754bf193da75d7ef950e610a6b361dae2dcb49d
Pierre Schmitz <pierre@archlinux.de>
2017-09-01 19:48:57 +0200

    Add Phan static code analyzer

---
+++ .phan/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'directory_list' => [
+        'src/',
+        'vendor/',
+        'tests/',
+    ],
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ]
+];
